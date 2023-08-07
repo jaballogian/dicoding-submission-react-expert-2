eda0831f98d1829a8788cb36ab05e749eef07b13
jaballogian <jaballogianrma77@gmail.com>
2023-08-07 14:14:30 +0700

    try to make the tests become error

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -6,7 +6,7 @@ function useInput(defaultValue = '') {
 
   const onValueChangeHandler = (event) => setValue(event.target.value);
 
-  return [value, onValueChangeHandler];
+  return [value];
 }
 
 export default useInput;
